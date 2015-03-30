4c32bb1f82ecdbe26de8c542ae5cbd8721e450aa
Javier Eguiluz <javier.eguiluz@gmail.com>
2015-03-30 18:01:30 +0200

    Added a new app:list-users command to show BufferedOutput + Table + Emails

---
+++ src/AppBundle/Command/ListUsersCommand.php
@@ -0,0 +1,133 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+/**
+ * A command console that lists all the existing users. To use this command, open
+ * a terminal window, enter into your project directory and execute the following:
+ *
+ *     $ php app/console app:list-users
+ *
+ * See http://symfony.com/doc/current/cookbook/console/console_command.html
+ *
+ * @author Javier Eguiluz <javier.eguiluz@gmail.com>
+ */
+class ListUsersCommand extends ContainerAwareCommand
+{
+    /** @var ObjectManager */
+    private $em;
+
+    protected function configure()
+    {
+        $this
+            // a good practice is to use the 'app:' prefix to group all your custom application commands
+            ->setName('app:list-users')
+            ->setDescription('Lists all the existing users')
+            ->setHelp(<<<HELP
+The <info>%command.name%</info> command lists all the users registered in the application:
+
+  <info>php %command.full_name%</info>
+
+By default the command only displays the 50 most recent users. Set the number of
+results to display with the <comment>--max-results</comment> option:
+
+  <info>php %command.full_name%</info> <comment>--max-results=2000</comment>
+
+In addition to displaying the user list, you can also send this information to
+the email address specified in the <comment>--send-to</comment> option:
+
+  <info>php %command.full_name%</info> <comment>--send-to=fabien@symfony.com</comment>
+
+HELP
+            )
+            // commands can optionally define arguments and/or options (mandatory and optional)
+            // see http://symfony.com/doc/current/components/console/console_arguments.html
+            ->addOption('max-results', null, InputOption::VALUE_OPTIONAL, 'Limits the number of users listed', 50)
+            ->addOption('send-to', null, InputOption::VALUE_OPTIONAL, 'If set, the result is sent to the given email address')
+        ;
+    }
+
+    /**
+     * This method is executed before the the execute() method. It's main purpose
+     * is to initialize the variables used in the rest of the command methods.
+     */
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->em = $this->getContainer()->get('doctrine')->getManager();
+    }
+
+    /**
+     * This method is executed after initialize(). It usually contains the logic
+     * to execute to complete this command task.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $maxResults = $input->getOption('max-results');
+        // Use ->findBy() instead of ->findAll() to allow result sorting and limiting
+        $users = $this->em->getRepository('AppBundle:User')->findBy(array(), array('id' => 'DESC'), $maxResults);
+
+        // Doctrine query returns an array of objects and we need an array of plain arrays
+        $usersAsPlainArrays = array_map(function ($user) {
+            return array($user->getId(), $user->getUsername(), $user->getEmail(), implode(', ', $user->getRoles()));
+        }, $users);
+
+        // Instead of the common output type, this command uses the BufferedOutput
+        // type, which allows to get the contents before displaying them in the
+        // console. You probably should never use this kind of output, because it's
+        // only suited for very special use cases, like the one proposed by this command
+        $bufferedOutput = new BufferedOutput();
+
+        $table = new Table($bufferedOutput);
+        $table
+            ->setHeaders(array('ID', 'Username', 'Email', 'Roles'))
+            ->setRows($usersAsPlainArrays)
+        ;
+        $table->render();
+
+        // instead of displaying the table of users, store it in a variable
+        $tableContents = $bufferedOutput->fetch();
+
+        if (null !== $email = $input->getOption('send-to')) {
+            $this->sendReport($tableContents, $email);
+        }
+
+        $output->writeln($tableContents);
+    }
+
+    /**
+     * Sends the given $contents to the $recipient email address.
+     *
+     * @param string $contents
+     * @param string $recipient
+     */
+    private function sendReport($contents, $recipient)
+    {
+        // See http://symfony.com/doc/current/cookbook/email/email.html
+        $mailer = $this->getContainer()->get('mailer');
+
+        $message = $mailer->createMessage()
+            ->setSubject(sprintf('app:list-users report (%s)', date('Y-m-d H:i:s')))
+            ->setFrom($this->getContainer()->getParameter('app.notifications.email_sender'))
+            ->setTo($recipient)
+            ->setBody(sprintf('<pre>%s</pre>', $contents), 'text/html')
+        ;
+
+        $mailer->send($message);
+    }
+}
