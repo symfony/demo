d26154471251fb6eff336fe36206250ef4ae0fe3
Javier Eguiluz <javier.eguiluz@gmail.com>
2015-04-21 12:26:51 +0200

    Improved the help notes of the production front controller

--- web/app.php
+++ web/app.php
@@ -9,30 +9,40 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // lines to use APC for class autoloading. This can improve application performance
 // very significantly. See http://symfony.com/doc/current/components/class_loader/cache_class_loader.html#apcclassloader
 //
+// NOTE: The first argument of ApcClassLoader() is a prefix that will be used to
+// prevent cache key conflicts. In a real Symfony application, make sure to change
+// it to a value that it's unique in the web server. A common practice is to use
+// the domain name associated to the Symfony application (e.g. 'example_com').
+//
 // $apcLoader = new ApcClassLoader('symfony_demo', $loader);
 // $loader->unregister();
 // $apcLoader->register(true);
-//
-// The first argument of ApcClassLoader() is a prefix that will be used to
-// prevent cache key conflicts. In a real Symfony application, make sure to change
-// it to a value that it's unique in the web server. A common practice is to use
-// the domain name associated to the Symfony application.
 
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 
-// If you use HTTP Cache to improve application performance, uncomment the following lines:
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
+
+// If you use HTTP Cache to improve application performance, comment the previous
+// lines (from 21st to 30th line) and uncomment the following lines:
 // See http://symfony.com/doc/current/book/http_cache.html#symfony-reverse-proxy
 //
+// require_once __DIR__.'/../app/AppKernel.php';
 // require_once __DIR__.'/../app/AppCache.php';
+
 // $kernel = new AppKernel('prod', false);
 // $kernel->loadClassCache();
 // $kernel = new AppCache($kernel);
 // Request::enableHttpMethodParameterOverride();
 
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+// $request = Request::createFromGlobals();
+// $response = $kernel->handle($request);
+// $response->send();
+
+// $kernel->terminate($request, $response);
