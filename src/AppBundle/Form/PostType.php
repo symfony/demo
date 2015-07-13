c6c9a03da07a8f272d1d9ff6a443dd15a64589c6
Javier Eguiluz <javier.eguiluz@gmail.com>
2015-07-13 15:56:35 +0200

    Improved the note about client-side and server-side validation

--- src/AppBundle/Form/PostType.php
+++ src/AppBundle/Form/PostType.php
@@ -29,8 +29,16 @@ class PostType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        // for the full reference of options defined by each form field type
+        // For the full reference of options defined by each form field type
         // see http://symfony.com/doc/current/reference/forms/types.html
+
+        // By default, form fields include the 'required' attribute, which enables
+        // the client-side form validation. This means that you can't test the
+        // server-side validation errors from the browser. To temporarily disable
+        // this validation, set the 'required' attribute to 'false':
+        //
+        //     $builder->add('title', null, array('required' => false, ...));
+
         $builder
             ->add('title', null, array('label' => 'label.title'))
             ->add('summary', 'textarea', array('label' => 'label.summary'))
