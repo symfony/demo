12c9efeadb4215a827535045602eab46f4ef9665
admin <mantulo@kaznetmedia.kz>
2015-08-02 14:23:50 +0600

    Removed not used namespace

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -12,7 +12,6 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 /**
