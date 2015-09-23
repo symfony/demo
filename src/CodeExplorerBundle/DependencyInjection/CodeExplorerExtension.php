44ea1a71850d78849d48de0eebfd25ca8bbae27d
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2015-09-23 22:06:14 +0300

    Fix a code style issue

--- src/CodeExplorerBundle/DependencyInjection/CodeExplorerExtension.php
+++ src/CodeExplorerBundle/DependencyInjection/CodeExplorerExtension.php
@@ -13,7 +13,7 @@ namespace CodeExplorerBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
@@ -23,7 +23,7 @@ class CodeExplorerExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 
         $loader->load('services.yml');
     }
