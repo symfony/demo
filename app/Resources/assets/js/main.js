d8376eac82a109e11641544da4b4aae4827e5d53
Yonel Ceruto <yonelceruto@gmail.com>
2015-10-26 02:34:42 -0400

    add a custom form field type example

--- app/Resources/assets/js/main.js
+++ app/Resources/assets/js/main.js
@@ -7,10 +7,28 @@
  * file that was distributed with this source code.
  */
 
-/**
- * Handling the modal confirmation message.
- */
 (function ($) {
+    $(document).ready(function () {
+        hljs.initHighlightingOnLoad();
+
+        // Datetime picker initialization.
+        // See http://eonasdan.github.io/bootstrap-datetimepicker/
+        $('[data-toggle="datetimepicker"]').datetimepicker({
+            icons: {
+                time: 'fa fa-clock-o',
+                date: 'fa fa-calendar',
+                up: 'fa fa-chevron-up',
+                down: 'fa fa-chevron-down',
+                previous: 'fa fa-chevron-left',
+                next: 'fa fa-chevron-right',
+                today: 'fa fa-check-circle-o',
+                clear: 'fa fa-trash',
+                close: 'fa fa-remove'
+            }
+        });
+    });
+
+    // Handling the modal confirmation message.
     $(document).on('submit', 'form[data-confirmation]', function (event) {
         var $form = $(this),
             $confirm = $('#confirmationModal');
