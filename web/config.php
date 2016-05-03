ecd01f4bed0cd111fb507cdcaaf01664b18b291a
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-05-03 11:37:58 +0200

    Updated dependencies for Symfony 3.0.5

--- web/config.php
+++ web/config.php
@@ -32,174 +32,174 @@ $minorProblems = $symfonyRequirements->getFailedRecommendations();
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <meta name="robots" content="noindex,nofollow" />
-    <title>Symfony Configuration Checker</title>
-    <link rel="stylesheet" href="bundles/framework/css/structure.css" media="all" />
-    <link rel="stylesheet" href="bundles/framework/css/body.css" media="all" />
-    <style type="text/css">
-        /* styles copied from bundles/sensiodistribution/webconfigurator/css/install.css */
-        body {
-            font-size: 14px;
-            font-family: "Lucida Sans Unicode", "Lucida Grande", Verdana, Arial, Helvetica, sans-serif;
-        }
-        .sf-reset h1.title {
-            font-size: 45px;
-            padding-bottom: 30px;
-        }
-        .sf-reset h2 {
-            font-weight: bold;
-            color: #FFFFFF;
-            /* Font is reset to sans-serif (like body) */
-            font-family: "Lucida Sans Unicode", "Lucida Grande", Verdana, Arial, Helvetica, sans-serif;
-            margin-bottom: 10px;
-            background-color: #aacd4e;
-            padding: 2px 4px;
-            display: inline-block;
-            text-transform: uppercase;
-        }
-        .sf-reset ul a,
-        .sf-reset ul a:hover {
-            background: url(../images/blue-arrow.png) no-repeat right 6px;
-            padding-right: 10px;
-        }
-        .sf-reset ul, ol {
-            padding-left: 20px;
-        }
-        .sf-reset li {
-            padding-bottom: 18px;
-        }
-        .sf-reset ol li {
-            list-style-type: decimal;
-        }
-        .sf-reset ul li {
-            list-style-type: none;
-        }
-        .sf-reset .symfony-blocks-install {
-            overflow: hidden;
-        }
-        .sf-reset .symfony-install-continue {
-            font-size: 0.95em;
-            padding-left: 0;
-        }
-        .sf-reset .symfony-install-continue li {
-            padding-bottom: 10px;
-        }
-        .sf-reset .ok {
-            color: #fff;
-            font-family: "Lucida Sans Unicode", "Lucida Grande", Verdana, Arial, Helvetica, sans-serif;
-            background-color: #6d6;
-            padding: 10px;
-            margin-bottom: 20px;
-        }
-        .sf-reset .ko {
-            background-color: #d66;
-        }
-        .version {
-            text-align: right;
-            font-size: 10px;
-            margin-right: 20px;
-        }
-        .sf-reset a,
-        .sf-reset li a {
-            color: #08C;
-            text-decoration: none;
-        }
-        .sf-reset a:hover,
-        .sf-reset li a:hover {
-            color: #08C;
-            text-decoration: underline;
-        }
-        .sf-reset textarea {
-            padding: 7px;
-        }
-    </style>
-</head>
-<body>
-<div id="content">
-    <div class="header clear-fix">
-        <div class="header-logo">
-            <img src="bundles/framework/images/logo_symfony.png" alt="Symfony" />
-        </div>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name="robots" content="noindex,nofollow" />
+        <title>Symfony Configuration Checker</title>
+        <link rel="stylesheet" href="bundles/framework/css/structure.css" media="all" />
+        <link rel="stylesheet" href="bundles/framework/css/body.css" media="all" />
+        <style type="text/css">
+            /* styles copied from bundles/sensiodistribution/webconfigurator/css/install.css */
+            body {
+                font-size: 14px;
+                font-family: "Lucida Sans Unicode", "Lucida Grande", Verdana, Arial, Helvetica, sans-serif;
+            }
+            .sf-reset h1.title {
+                font-size: 45px;
+                padding-bottom: 30px;
+            }
+            .sf-reset h2 {
+                font-weight: bold;
+                color: #FFFFFF;
+                /* Font is reset to sans-serif (like body) */
+                font-family: "Lucida Sans Unicode", "Lucida Grande", Verdana, Arial, Helvetica, sans-serif;
+                margin-bottom: 10px;
+                background-color: #aacd4e;
+                padding: 2px 4px;
+                display: inline-block;
+                text-transform: uppercase;
+            }
+            .sf-reset ul a,
+            .sf-reset ul a:hover {
+                background: url(../images/blue-arrow.png) no-repeat right 6px;
+                padding-right: 10px;
+            }
+            .sf-reset ul, ol {
+                padding-left: 20px;
+            }
+            .sf-reset li {
+                padding-bottom: 18px;
+            }
+            .sf-reset ol li {
+                list-style-type: decimal;
+            }
+            .sf-reset ul li {
+                list-style-type: none;
+            }
+            .sf-reset .symfony-blocks-install {
+                overflow: hidden;
+            }
+            .sf-reset .symfony-install-continue {
+                font-size: 0.95em;
+                padding-left: 0;
+            }
+            .sf-reset .symfony-install-continue li {
+                padding-bottom: 10px;
+            }
+            .sf-reset .ok {
+                color: #fff;
+                font-family: "Lucida Sans Unicode", "Lucida Grande", Verdana, Arial, Helvetica, sans-serif;
+                background-color: #6d6;
+                padding: 10px;
+                margin-bottom: 20px;
+            }
+            .sf-reset .ko {
+                background-color: #d66;
+            }
+            .version {
+                text-align: right;
+                font-size: 10px;
+                margin-right: 20px;
+            }
+            .sf-reset a,
+            .sf-reset li a {
+                color: #08C;
+                text-decoration: none;
+            }
+            .sf-reset a:hover,
+            .sf-reset li a:hover {
+                color: #08C;
+                text-decoration: underline;
+            }
+            .sf-reset textarea {
+                padding: 7px;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="content">
+            <div class="header clear-fix">
+                <div class="header-logo">
+                    <img src="bundles/framework/images/logo_symfony.png" alt="Symfony" />
+                </div>
 
-        <div class="search">
-            <form method="get" action="http://symfony.com/search">
-                <div class="form-row">
+                <div class="search">
+                  <form method="get" action="http://symfony.com/search">
+                    <div class="form-row">
 
-                    <label for="search-id">
-                        <img src="bundles/framework/images/grey_magnifier.png" alt="Search on Symfony website" />
-                    </label>
+                      <label for="search-id">
+                          <img src="bundles/framework/images/grey_magnifier.png" alt="Search on Symfony website" />
+                      </label>
 
-                    <input name="q" id="search-id" type="search" placeholder="Search on Symfony website" />
+                      <input name="q" id="search-id" type="search" placeholder="Search on Symfony website" />
 
-                    <button type="submit" class="sf-button">
+                      <button type="submit" class="sf-button">
                           <span class="border-l">
                             <span class="border-r">
                                 <span class="btn-bg">OK</span>
                             </span>
                         </span>
-                    </button>
+                      </button>
+                    </div>
+                   </form>
                 </div>
-            </form>
-        </div>
-    </div>
-
-    <div class="sf-reset">
-        <div class="block">
-            <div class="symfony-block-content">
-                <h1 class="title">Configuration Checker</h1>
-                <p>
-                    This script analyzes your system to check whether is
-                    ready to run Symfony applications.
-                </p>
-
-                <?php if (count($majorProblems)): ?>
-                    <h2 class="ko">Major problems</h2>
-                    <p>Major problems have been detected and <strong>must</strong> be fixed before continuing:</p>
-                    <ol>
-                        <?php foreach ($majorProblems as $problem): ?>
-                            <li><?php echo $problem->getHelpHtml() ?></li>
-                        <?php endforeach; ?>
-                    </ol>
-                <?php endif; ?>
-
-                <?php if (count($minorProblems)): ?>
-                    <h2>Recommendations</h2>
-                    <p>
-                        <?php if (count($majorProblems)): ?>Additionally, to<?php else: ?>To<?php endif; ?> enhance your Symfony experience,
-                        it’s recommended that you fix the following:
-                    </p>
-                    <ol>
-                        <?php foreach ($minorProblems as $problem): ?>
-                            <li><?php echo $problem->getHelpHtml() ?></li>
-                        <?php endforeach; ?>
-                    </ol>
-                <?php endif; ?>
-
-                <?php if ($symfonyRequirements->hasPhpIniConfigIssue()): ?>
-                    <p id="phpini">*
-                        <?php if ($symfonyRequirements->getPhpIniConfigPath()): ?>
-                            Changes to the <strong>php.ini</strong> file must be done in "<strong><?php echo $symfonyRequirements->getPhpIniConfigPath() ?></strong>".
-                        <?php else: ?>
-                            To change settings, create a "<strong>php.ini</strong>".
-                        <?php endif; ?>
-                    </p>
-                <?php endif; ?>
-
-                <?php if (!count($majorProblems) && !count($minorProblems)): ?>
-                    <p class="ok">All checks passed successfully. Your system is ready to run Symfony applications.</p>
-                <?php endif; ?>
-
-                <ul class="symfony-install-continue">
-                    <?php if (count($majorProblems) || count($minorProblems)): ?>
-                        <li><a href="config.php">Re-check configuration</a></li>
-                    <?php endif; ?>
-                </ul>
             </div>
+
+            <div class="sf-reset">
+                <div class="block">
+                    <div class="symfony-block-content">
+                        <h1 class="title">Configuration Checker</h1>
+                        <p>
+                            This script analyzes your system to check whether is
+                            ready to run Symfony applications.
+                        </p>
+
+                        <?php if (count($majorProblems)): ?>
+                            <h2 class="ko">Major problems</h2>
+                            <p>Major problems have been detected and <strong>must</strong> be fixed before continuing:</p>
+                            <ol>
+                                <?php foreach ($majorProblems as $problem): ?>
+                                    <li><?php echo $problem->getHelpHtml() ?></li>
+                                <?php endforeach; ?>
+                            </ol>
+                        <?php endif; ?>
+
+                        <?php if (count($minorProblems)): ?>
+                            <h2>Recommendations</h2>
+                            <p>
+                                <?php if (count($majorProblems)): ?>Additionally, to<?php else: ?>To<?php endif; ?> enhance your Symfony experience,
+                                it’s recommended that you fix the following:
+                            </p>
+                            <ol>
+                                <?php foreach ($minorProblems as $problem): ?>
+                                    <li><?php echo $problem->getHelpHtml() ?></li>
+                                <?php endforeach; ?>
+                            </ol>
+                        <?php endif; ?>
+
+                        <?php if ($symfonyRequirements->hasPhpIniConfigIssue()): ?>
+                            <p id="phpini">*
+                                <?php if ($symfonyRequirements->getPhpIniConfigPath()): ?>
+                                    Changes to the <strong>php.ini</strong> file must be done in "<strong><?php echo $symfonyRequirements->getPhpIniConfigPath() ?></strong>".
+                                <?php else: ?>
+                                    To change settings, create a "<strong>php.ini</strong>".
+                                <?php endif; ?>
+                            </p>
+                        <?php endif; ?>
+
+                        <?php if (!count($majorProblems) && !count($minorProblems)): ?>
+                            <p class="ok">All checks passed successfully. Your system is ready to run Symfony applications.</p>
+                        <?php endif; ?>
+
+                        <ul class="symfony-install-continue">
+                            <?php if (count($majorProblems) || count($minorProblems)): ?>
+                                <li><a href="config.php">Re-check configuration</a></li>
+                            <?php endif; ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="version">Symfony Standard Edition</div>
         </div>
-    </div>
-    <div class="version">Symfony Standard Edition</div>
-</div>
-</body>
+    </body>
 </html>
