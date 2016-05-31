56cfa665cf1235c436c70afa5fc85c2668c14605
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-05-31 10:57:23 +0200

    Upgrade the Demo Application to Symfony 3.1

--- app/autoload.php
+++ app/autoload.php
@@ -3,13 +3,11 @@
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
-error_reporting(error_reporting() & ~E_USER_DEPRECATED);
-
 /**
  * @var ClassLoader $loader
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
 return $loader;
