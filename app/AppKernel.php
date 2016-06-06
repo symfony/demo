951e84c0ca41c7daea82a039b92ac185265d325f
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-06-06 09:38:30 +0200

    Updated the logs and cache dirs location

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -40,6 +40,21 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
+
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
