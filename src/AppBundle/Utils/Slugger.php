2df12de701a5d8497661e80af60976e6b4667f2b
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-10-13 16:15:07 +0200

    Made the slugger compatible with every language

--- src/AppBundle/Utils/Slugger.php
+++ src/AppBundle/Utils/Slugger.php
@@ -27,6 +27,6 @@ class Slugger
      */
     public function slugify($string)
     {
-        return trim(preg_replace('/[^a-z0-9]+/', '-', strtolower(strip_tags($string))), '-');
+        return str_replace(' ', '-', strtolower(trim(strip_tags($string))));
     }
 }
