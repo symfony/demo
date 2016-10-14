05447f03ceac733e6010e1ac44dbd4acd3d939d4
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-10-14 16:51:47 +0200

    Force the internal encoding to UTF-8

--- src/AppBundle/Utils/Slugger.php
+++ src/AppBundle/Utils/Slugger.php
@@ -27,6 +27,6 @@ class Slugger
      */
     public function slugify($string)
     {
-        return preg_replace('/\s+/', '-', mb_strtolower(trim(strip_tags($string))));
+        return preg_replace('/\s+/', '-', mb_strtolower(trim(strip_tags($string)), 'UTF-8'));
     }
 }
