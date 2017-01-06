88b4227bec0bec55b3522fbe5d6ee3aa911e58b7
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-01-06 09:35:56 +0100

    Added some help notes

--- src/AppBundle/EventListener/ConsoleEventSubscriber.php
+++ src/AppBundle/EventListener/ConsoleEventSubscriber.php
@@ -16,16 +16,37 @@ use Symfony\Component\Console\Event\ConsoleExceptionEvent;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ * This application uses by default a SQLite database to store its information.
+ * That's why the 'sqlite3' extension must be enabled in PHP. This event
+ * subscriber listens to console events and in case of an exception caused by
+ * a disabled 'sqlite3' extension, it displays a meaningful error message.
+ *
+ * @author Javier Eguiluz <javier.eguiluz@gmail.com>
+ */
 class ConsoleEventSubscriber implements EventSubscriberInterface
 {
+    // Event Subscribers must define this method to declare the events they
+    // listen to. You can listen to several events, execute more than one method
+    // for each event and set the priority of each event too.
+    // See http://symfony.com/doc/current/event_dispatcher.html#creating-an-event-subscriber
     public static function getSubscribedEvents()
     {
         return [
-            ConsoleEvents::EXCEPTION => 'displayBetterErrorMessages',
+            // Exceptions are one of the events defined by the Console. See the
+            // rest here: http://symfony.com/doc/current/components/console/events.html
+            ConsoleEvents::EXCEPTION => 'handleDatabaseExceptions',
         ];
     }
 
-    public function displayBetterErrorMessages(ConsoleExceptionEvent $event)
+    /**
+     * This method checks if there has been an exception in a command related to
+     * the database and then, it checks if the 'sqlite3' PHP extension is enabled
+     * or not to display a better error message.
+     *
+     * @param ConsoleExceptionEvent $event
+     */
+    public function handleDatabaseExceptions(ConsoleExceptionEvent $event)
     {
         $commandNames = ['doctrine:fixtures:load', 'doctrine:database:create', 'doctrine:schema:create', 'doctrine:database:drop'];
 
