48a433ac4adaf9f162fa11164e749d2c91fc6add
Yonel Ceruto <yonelceruto@gmail.com>
2017-02-08 12:00:39 -0500

    Updated fixtures and db

--- src/AppBundle/DataFixtures/ORM/UserFixtures.php
+++ src/AppBundle/DataFixtures/ORM/UserFixtures.php
@@ -40,19 +40,21 @@ class UserFixtures extends AbstractFixture implements ContainerAwareInterface
     {
         $passwordEncoder = $this->container->get('security.password_encoder');
 
-        $annaAdmin = new User();
-        $annaAdmin->setUsername('anna_admin');
-        $annaAdmin->setEmail('anna_admin@symfony.com');
-        $annaAdmin->setRoles(['ROLE_ADMIN']);
-        $encodedPassword = $passwordEncoder->encodePassword($annaAdmin, 'kitten');
-        $annaAdmin->setPassword($encodedPassword);
-        $manager->persist($annaAdmin);
+        $janeAdmin = new User();
+        $janeAdmin->setFullName('Jane Doe');
+        $janeAdmin->setUsername('jane_admin');
+        $janeAdmin->setEmail('jane_admin@symfony.com');
+        $janeAdmin->setRoles(['ROLE_ADMIN']);
+        $encodedPassword = $passwordEncoder->encodePassword($janeAdmin, 'kitten');
+        $janeAdmin->setPassword($encodedPassword);
+        $manager->persist($janeAdmin);
         // In case if fixture objects have relations to other fixtures, adds a reference
         // to that object by name and later reference it to form a relation.
         // See https://symfony.com/doc/current/bundles/DoctrineFixturesBundle/index.html#sharing-objects-between-fixtures
-        $this->addReference('anna-admin', $annaAdmin);
+        $this->addReference('jane-admin', $janeAdmin);
 
         $johnUser = new User();
+        $johnUser->setFullName('John Doe');
         $johnUser->setUsername('john_user');
         $johnUser->setEmail('john_user@symfony.com');
         $encodedPassword = $passwordEncoder->encodePassword($johnUser, 'kitten');
