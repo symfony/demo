81694ac21e63bebe7ecfa22fa689766f2f38ccd5
Hugo Hamon <hugo.hamon@sensiolabs.com>
2017-02-21 16:25:59 +0100

    [app] load WebServerBundle and SensioGeneratorBundle only in dev environment.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,9 +32,12 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), ['dev', 'test'])) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+
+            if ('dev' === $this->getEnvironment()) {
+                $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            }
 
             if ('test' === $this->getEnvironment()) {
                 // this bundle makes it easier to work with databases in PHPUnit
