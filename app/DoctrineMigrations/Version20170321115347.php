3b22b05121f2b64c9cf38b1c9a8a69f2a2a10755
apetitpa <arnaud@easyloisirs.com>
2017-03-22 10:39:28 +0100

    Added an example of a database migration
    As suggested in #512, this adds an example of how to use the DoctrineMigrationBundle

---
+++ app/DoctrineMigrations/Version20170321115347.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * This is an example of a database migration using the DoctrineMigrationsBundle.
+ * Here we add a new column to the symfony_demo_user table matching the fullName User property.
+ *
+ * @see http://symfony.com/doc/master/bundles/DoctrineMigrationsBundle/index.html
+ *
+ * @author Arnaud PETITPAS <arnaudpetitpas@protonmail.ch>
+ */
+class Version20170321115347 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE symfony_demo_user ADD fullName VARCHAR(255) DEFAULT NULL');
+        $this->addSql('UPDATE symfony_demo_user SET fullName = username');
+        $this->addSql('ALTER TABLE symfony_demo_user CHANGE fullName fullName VARCHAR(255) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE symfony_demo_user DROP fullName');
+    }
+}
