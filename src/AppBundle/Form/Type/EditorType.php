c0819ecd108ca07336f8b0502ddc996f0316fc5d
Nikolai Plath <der.el.kuku@gmail.com>
2017-04-08 13:22:56 -0500

    Add editor field type

---
+++ src/AppBundle/Form/Type/EditorType.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+
+/**
+ * Defines a custom form field type used to display a markdown editor with preview functionality.
+ *
+ * See http://symfony.com/doc/current/cookbook/form/create_custom_field_type.html
+ *
+ * @author Nikolai Plath <der.el.kuku@gmail.com>
+ */
+class EditorType extends AbstractType
+{
+}
