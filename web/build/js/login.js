3435c2d110e003184be04952463047aa679526b2
Christophe Coevoet <stof@notk.org>
2017-06-14 10:34:08 +0200

    Commit missing built asset files

---
+++ web/build/js/login.js
@@ -0,0 +1,4 @@
+webpackJsonp([5],{20:/*!******************************************!*\
+  !*** ./app/Resources/assets/js/login.js ***!
+  \******************************************/
+function(a,n,l){(function(a){a(function(){var n=a("#username"),l=a("#password");n.val()||l.val()||(n.val("jane_admin"),l.val("kitten"))})}).call(n,l(/*! jquery */0))}},[20]);
