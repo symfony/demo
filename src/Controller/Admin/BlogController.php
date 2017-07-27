22dc5b92c41d13d9bc62d4f3fad85b647ab4e1d8
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-07-27 15:38:36 +0200

    Removed bundles

---
+++ src/Controller/Admin/BlogController.php
@@ -0,0 +1,189 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller\Admin;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Utils\Slugger;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Controller used to manage blog contents in the backend.
+ *
+ * Please note that the application backend is developed manually for learning
+ * purposes. However, in your real Symfony application you should use any of the
+ * existing bundles that let you generate ready-to-use backends without effort.
+ *
+ * See http://knpbundles.com/keyword/admin
+ *
+ * @Route("/admin/post")
+ * @Security("has_role('ROLE_ADMIN')")
+ *
+ * @author Ryan Weaver <weaverryan@gmail.com>
+ * @author Javier Eguiluz <javier.eguiluz@gmail.com>
+ */
+class BlogController extends Controller
+{
+    /**
+     * Lists all Post entities.
+     *
+     * This controller responds to two different routes with the same URL:
+     *   * 'admin_post_index' is the route with a name that follows the same
+     *     structure as the rest of the controllers of this class.
+     *   * 'admin_index' is a nice shortcut to the backend homepage. This allows
+     *     to create simpler links in the templates. Moreover, in the future we
+     *     could move this annotation to any other controller while maintaining
+     *     the route name and therefore, without breaking any existing link.
+     *
+     * @Route("/", name="admin_index")
+     * @Route("/", name="admin_post_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $posts = $em->getRepository(Post::class)->findBy(['author' => $this->getUser()], ['publishedAt' => 'DESC']);
+
+        return $this->render('admin/blog/index.html.twig', ['posts' => $posts]);
+    }
+
+    /**
+     * Creates a new Post entity.
+     *
+     * @Route("/new", name="admin_post_new")
+     * @Method({"GET", "POST"})
+     *
+     * NOTE: the Method annotation is optional, but it's a recommended practice
+     * to constraint the HTTP methods each controller responds to (by default
+     * it responds to all methods).
+     */
+    public function newAction(Request $request, Slugger $slugger)
+    {
+        $post = new Post();
+        $post->setAuthor($this->getUser());
+
+        // See https://symfony.com/doc/current/book/forms.html#submitting-forms-with-multiple-buttons
+        $form = $this->createForm(PostType::class, $post)
+            ->add('saveAndCreateNew', SubmitType::class);
+
+        $form->handleRequest($request);
+
+        // the isSubmitted() method is completely optional because the other
+        // isValid() method already checks whether the form is submitted.
+        // However, we explicitly add it to improve code readability.
+        // See https://symfony.com/doc/current/best_practices/forms.html#handling-form-submits
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setSlug($slugger->slugify($post->getTitle()));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            // Flash messages are used to notify the user about the result of the
+            // actions. They are deleted automatically from the session as soon
+            // as they are accessed.
+            // See https://symfony.com/doc/current/book/controller.html#flash-messages
+            $this->addFlash('success', 'post.created_successfully');
+
+            if ($form->get('saveAndCreateNew')->isClicked()) {
+                return $this->redirectToRoute('admin_post_new');
+            }
+
+            return $this->redirectToRoute('admin_post_index');
+        }
+
+        return $this->render('admin/blog/new.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Finds and displays a Post entity.
+     *
+     * @Route("/{id}", requirements={"id": "\d+"}, name="admin_post_show")
+     * @Method("GET")
+     */
+    public function showAction(Post $post)
+    {
+        // This security check can also be performed
+        // using an annotation: @Security("is_granted('show', post)")
+        $this->denyAccessUnlessGranted('show', $post, 'Posts can only be shown to their authors.');
+
+        return $this->render('admin/blog/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    /**
+     * Displays a form to edit an existing Post entity.
+     *
+     * @Route("/{id}/edit", requirements={"id": "\d+"}, name="admin_post_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Post $post, Slugger $slugger)
+    {
+        $this->denyAccessUnlessGranted('edit', $post, 'Posts can only be edited by their authors.');
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setSlug($slugger->slugify($post->getTitle()));
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', 'post.updated_successfully');
+
+            return $this->redirectToRoute('admin_post_edit', ['id' => $post->getId()]);
+        }
+
+        return $this->render('admin/blog/edit.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Deletes a Post entity.
+     *
+     * @Route("/{id}/delete", name="admin_post_delete")
+     * @Method("POST")
+     * @Security("is_granted('delete', post)")
+     *
+     * The Security annotation value is an expression (if it evaluates to false,
+     * the authorization mechanism will prevent the user accessing this resource).
+     */
+    public function deleteAction(Request $request, Post $post)
+    {
+        if (!$this->isCsrfTokenValid('delete', $request->request->get('token'))) {
+            return $this->redirectToRoute('admin_post_index');
+        }
+
+        // Delete the tags associated with this blog post. This is done automatically
+        // by Doctrine, except for SQLite (the database used in this application)
+        // because foreign key support is not enabled by default in SQLite
+        $post->getTags()->clear();
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+
+        $this->addFlash('success', 'post.deleted_successfully');
+
+        return $this->redirectToRoute('admin_post_index');
+    }
+}
---
+++ src/Controller/BlogController.php
@@ -0,0 +1,181 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Events;
+use App\Form\CommentType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Controller used to manage blog contents in the public part of the site.
+ *
+ * @Route("/blog")
+ *
+ * @author Ryan Weaver <weaverryan@gmail.com>
+ * @author Javier Eguiluz <javier.eguiluz@gmail.com>
+ */
+class BlogController extends Controller
+{
+    /**
+     * @Route("/", defaults={"page": "1", "_format"="html"}, name="blog_index")
+     * @Route("/rss.xml", defaults={"page": "1", "_format"="xml"}, name="blog_rss")
+     * @Route("/page/{page}", defaults={"_format"="html"}, requirements={"page": "[1-9]\d*"}, name="blog_index_paginated")
+     * @Method("GET")
+     * @Cache(smaxage="10")
+     *
+     * NOTE: For standard formats, Symfony will also automatically choose the best
+     * Content-Type header for the response.
+     * See https://symfony.com/doc/current/quick_tour/the_controller.html#using-formats
+     */
+    public function indexAction($page, $_format)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $posts = $em->getRepository(Post::class)->findLatest($page);
+
+        // Every template name also has two extensions that specify the format and
+        // engine for that template.
+        // See https://symfony.com/doc/current/templating.html#template-suffix
+        return $this->render('blog/index.'.$_format.'.twig', ['posts' => $posts]);
+    }
+
+    /**
+     * @Route("/posts/{slug}", name="blog_post")
+     * @Method("GET")
+     *
+     * NOTE: The $post controller argument is automatically injected by Symfony
+     * after performing a database query looking for a Post with the 'slug'
+     * value given in the route.
+     * See https://symfony.com/doc/current/bundles/SensioFrameworkExtraBundle/annotations/converters.html
+     */
+    public function postShowAction(Post $post)
+    {
+        // Symfony provides a function called 'dump()' which is an improved version
+        // of the 'var_dump()' function. It's useful to quickly debug the contents
+        // of any variable, but it's not available in the 'prod' environment to
+        // prevent any leak of sensitive information.
+        // This function can be used both in PHP files and Twig templates. The only
+        // requirement is to have enabled the DebugBundle.
+        if ('dev' === $this->getParameter('kernel.environment')) {
+            dump($post, $this->getUser(), new \DateTime());
+        }
+
+        return $this->render('blog/post_show.html.twig', ['post' => $post]);
+    }
+
+    /**
+     * @Route("/comment/{postSlug}/new", name="comment_new")
+     * @Method("POST")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     * @ParamConverter("post", options={"mapping": {"postSlug": "slug"}})
+     *
+     * NOTE: The ParamConverter mapping is required because the route parameter
+     * (postSlug) doesn't match any of the Doctrine entity properties (slug).
+     * See https://symfony.com/doc/current/bundles/SensioFrameworkExtraBundle/annotations/converters.html#doctrine-converter
+     */
+    public function commentNewAction(Request $request, Post $post, EventDispatcherInterface $eventDispatcher)
+    {
+        $comment = new Comment();
+        $comment->setAuthor($this->getUser());
+        $post->addComment($comment);
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($comment);
+            $em->flush();
+
+            // When triggering an event, you can optionally pass some information.
+            // For simple applications, use the GenericEvent object provided by Symfony
+            // to pass some PHP variables. For more complex applications, define your
+            // own event object classes.
+            // See https://symfony.com/doc/current/components/event_dispatcher/generic_event.html
+            $event = new GenericEvent($comment);
+
+            // When an event is dispatched, Symfony notifies it to all the listeners
+            // and subscribers registered to it. Listeners can modify the information
+            // passed in the event and they can even modify the execution flow, so
+            // there's no guarantee that the rest of this controller will be executed.
+            // See https://symfony.com/doc/current/components/event_dispatcher.html
+            $eventDispatcher->dispatch(Events::COMMENT_CREATED, $event);
+
+            return $this->redirectToRoute('blog_post', ['slug' => $post->getSlug()]);
+        }
+
+        return $this->render('blog/comment_form_error.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * This controller is called directly via the render() function in the
+     * blog/post_show.html.twig template. That's why it's not needed to define
+     * a route name for it.
+     *
+     * The "id" of the Post is passed in and then turned into a Post object
+     * automatically by the ParamConverter.
+     *
+     * @param Post $post
+     *
+     * @return Response
+     */
+    public function commentFormAction(Post $post)
+    {
+        $form = $this->createForm(CommentType::class);
+
+        return $this->render('blog/_comment_form.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/search", name="blog_search")
+     * @Method("GET")
+     *
+     * @return Response|JsonResponse
+     */
+    public function searchAction(Request $request)
+    {
+        if (!$request->isXmlHttpRequest()) {
+            return $this->render('blog/search.html.twig');
+        }
+
+        $query = $request->query->get('q', '');
+        $posts = $this->getDoctrine()->getRepository(Post::class)->findBySearchQuery($query);
+
+        $results = [];
+        foreach ($posts as $post) {
+            $results[] = [
+                'title' => htmlspecialchars($post->getTitle()),
+                'summary' => htmlspecialchars($post->getSummary()),
+                'url' => $this->generateUrl('blog_post', ['slug' => $post->getSlug()]),
+            ];
+        }
+
+        return $this->json($results);
+    }
+}
