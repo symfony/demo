4c695dac6e778741af75a0ce3e36e4b91bf8f39e
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-07-27 15:20:05 +0200

    Updated front controllers

---
+++ web/index.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+require __DIR__.'/../vendor/autoload.php';
+
+// The check is to ensure we don't use .env in production
+if (!getenv('APP_ENV')) {
+    (new Dotenv())->load(__DIR__.'/../.env');
+}
+
+if (getenv('APP_DEBUG')) {
+    // WARNING: You should setup permissions the proper way!
+    // REMOVE the following PHP line and read
+    // https://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
+    umask(0000);
+
+    Debug::enable();
+}
+
+// Request::setTrustedProxies(['0.0.0.0/0'], Request::HEADER_FORWARDED);
+
+$kernel = new Kernel(getenv('APP_ENV'), getenv('APP_DEBUG'));
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
