c574cb35c3438ac2f99b0f28050b8d946c040480
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-07-30 16:35:55 +0200

    Fixed some Travis CI issues

--- public/index.php
+++ public/index.php
@@ -1,9 +1,18 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use App\Kernel;
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Debug\Debug;
 
 require __DIR__.'/../vendor/autoload.php';
 
