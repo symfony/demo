6c687436f9db37a8a8fba241e126fe6497ec1098
Yonel Ceruto <yonelceruto@gmail.com>
2017-08-30 07:51:29 -0400

    Don't use getenv(), use $_SERVER instead

--- public/index.php
+++ public/index.php
@@ -17,11 +17,11 @@ use Symfony\Component\HttpFoundation\Request;
 require __DIR__.'/../vendor/autoload.php';
 
 // The check is to ensure we don't use .env in production
-if (!getenv('APP_ENV')) {
+if (!isset($_SERVER['APP_ENV'])) {
     (new Dotenv())->load(__DIR__.'/../.env');
 }
 
-if (getenv('APP_DEBUG')) {
+if ($_SERVER['APP_DEBUG'] ?? true) {
     // WARNING: You should setup permissions the proper way!
     // REMOVE the following PHP line and read
     // https://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
@@ -32,7 +32,7 @@ if (getenv('APP_DEBUG')) {
 
 // Request::setTrustedProxies(['0.0.0.0/0'], Request::HEADER_FORWARDED);
 
-$kernel = new Kernel(getenv('APP_ENV'), getenv('APP_DEBUG'));
+$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'dev', $_SERVER['APP_DEBUG'] ?? true);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
