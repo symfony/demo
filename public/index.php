12944ec916698a282a727d20571f968083dd43a1
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-11-10 09:44:20 +0100

    Restored a PHPdoc block to make PHP-CS-Fixer pass

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
