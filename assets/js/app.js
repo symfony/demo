10a7ba8a387a9e3d8d9b00167e155c529a2a1551
ptrm04 <peter.mikuta@gmail.com>
2018-01-08 02:03:22 +0800

    Fixing a typo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 // loads the Bootstrap jQuery plugins
 import 'bootstrap-sass/assets/javascripts/bootstrap/transition.js';
-import 'bootstrap-sass/assets/javascripts/bootstrap/alerts.js';
+import 'bootstrap-sass/assets/javascripts/bootstrap/alert.js';
 import 'bootstrap-sass/assets/javascripts/bootstrap/collapse.js';
 import 'bootstrap-sass/assets/javascripts/bootstrap/dropdown.js';
 import 'bootstrap-sass/assets/javascripts/bootstrap/modal.js';
