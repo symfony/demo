364c6b009b4f2dd9ba05af22172eb99317457d80
ptrm04 <peter.mikuta@gmail.com>
2018-01-08 01:52:00 +0800

    Including alerts.js and changing loading order
    Alerts currently can't be dismissed in the symfony-demo app.
    Also, updating the loading order to a correct one: https://github.com/twbs/bootstrap-sass#javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,9 @@
 // loads the Bootstrap jQuery plugins
+import 'bootstrap-sass/assets/javascripts/bootstrap/transition.js';
+import 'bootstrap-sass/assets/javascripts/bootstrap/alerts.js';
 import 'bootstrap-sass/assets/javascripts/bootstrap/collapse.js';
 import 'bootstrap-sass/assets/javascripts/bootstrap/dropdown.js';
 import 'bootstrap-sass/assets/javascripts/bootstrap/modal.js';
-import 'bootstrap-sass/assets/javascripts/bootstrap/transition.js';
 
 // loads the code syntax highlighting library
 import './highlight.js';
