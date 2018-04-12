5d9c3cefcbd969af6a216370b1dae87eec3e7308
Martin Aarhof <martin.aarhof@gmail.com>
2018-04-12 19:39:53 +0200

    Cleaned up bootstrap and changed comments publisheddate on fixtures to before "now"

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,28 +2,25 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use App\Kernel;
-use Doctrine\Bundle\MigrationsBundle\Command\MigrationsMigrateDoctrineCommand;
 use Symfony\Component\Console\Input\ArrayInput;
 
 $kernel = new Kernel('test', true);
 $kernel->boot();
 $application = new \Symfony\Bundle\FrameworkBundle\Console\Application($kernel);
 $application->setAutoExit(false);
-$application->add(new \Doctrine\Bundle\DoctrineBundle\Command\DropDatabaseDoctrineCommand());
-$application->add(new \Doctrine\Bundle\DoctrineBundle\Command\CreateDatabaseDoctrineCommand());
-$application->add(new \Doctrine\Bundle\DoctrineBundle\Command\Proxy\RunSqlDoctrineCommand());
-$application->add(new MigrationsMigrateDoctrineCommand());
 $application->run(new \Symfony\Component\Console\Input\ArrayInput([
     'command' => 'doctrine:database:drop',
     '--if-exists' => '1',
     '--force' => '1',
+    '-q' => '1',
 ]));
 $application->run(new \Symfony\Component\Console\Input\ArrayInput([
     'command' => 'doctrine:database:create',
+    '-q' => '1',
 ]));
 $application->run(new ArrayInput([
     'command' => 'doctrine:migrations:migrate',
     '--no-interaction' => '1',
-    '--quiet' => '1'
+    '-q' => '1',
 ]));
 $kernel->shutdown();
