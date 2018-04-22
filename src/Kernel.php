01e78c0d183723d704058b7c0ca4959dd627e75a
Javier Eguiluz <javier.eguiluz@gmail.com>
2018-04-22 20:13:33 +0200

    Added missing import

--- src/Kernel.php
+++ src/Kernel.php
@@ -13,6 +13,7 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
