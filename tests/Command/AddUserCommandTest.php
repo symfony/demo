72eb366f88e97599b036faccd7dc1daac936a59c
Javier Eguiluz <javier.eguiluz@gmail.com>
2018-06-29 12:25:17 +0200

    Use a fully-qualified PHP_OS_FAMILY constant

--- tests/Command/AddUserCommandTest.php
+++ tests/Command/AddUserCommandTest.php
@@ -33,7 +33,7 @@ class AddUserCommandTest extends KernelTestCase
         exec('stty 2>&1', $output, $exitcode);
         $isSttySupported = 0 === $exitcode;
 
-        if ('Windows' === PHP_OS_FAMILY || !$isSttySupported) {
+        if ('Windows' === \PHP_OS_FAMILY || !$isSttySupported) {
             $this->markTestSkipped('`stty` is required to test this command.');
         }
     }
