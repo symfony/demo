39f0d9ce537a5614e901e0848b833e79dbb29227
Javier Eguiluz <javier.eguiluz@gmail.com>
2018-11-30 11:45:26 +0100

    Updated contents for the latest Symfony Flex recipes

--- src/Kernel.php
+++ src/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends BaseKernel
     {
         $contents = require $this->getProjectDir().'/config/bundles.php';
         foreach ($contents as $class => $envs) {
-            if (isset($envs['all']) || isset($envs[$this->environment])) {
+            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
                 yield new $class();
             }
         }
