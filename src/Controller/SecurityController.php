24f825eadf56927313a546efb8ffb8b6232f5aa7
Javier Eguiluz <javier.eguiluz@gmail.com>
2019-04-29 15:46:18 +0200

    Refactored code

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,12 +12,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Security\Core\Security;
 
 /**
  * Controller used to manage the application security.
@@ -33,10 +33,10 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="security_login")
      */
-    public function login(Request $request, AuthenticationUtils $helper): Response
+    public function login(Request $request, Security $security, AuthenticationUtils $helper): Response
     {
-        // Block login page for logged in users
-        if ($this->container->get('security.authorization_checker')->isGranted('ROLE_USER')) {
+        // if user is already logged in, don't display the login page again
+        if ($security->isGranted('ROLE_USER')) {
             return $this->redirectToRoute('blog_index');
         }
 
