7590f6f859f627559fa52426cdd8fe1d423193a6
Javier Eguiluz <javier.eguiluz@gmail.com>
2019-05-02 09:55:15 +0200

    Fixed a minor code syntax issue

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,9 +15,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
-use Symfony\Component\Security\Core\Security;
 
 /**
  * Controller used to manage the application security.
