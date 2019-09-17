7c156394a6dbb79ed9f849909ecdc81e2c1007d6
abdounikarim <abdounikarim@gmail.com>
2019-09-18 00:25:22 +0200

    correcting templates

--- public/build/login.js
+++ public/build/login.js
@@ -1,14 +1,1 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["login"],{
-
-/***/ "./assets/js/login.js":
-/*!****************************!*\
-  !*** ./assets/js/login.js ***!
-  \****************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-eval("/* WEBPACK VAR INJECTION */(function($) {$(function () {\n  var usernameEl = $('#username');\n  var passwordEl = $('#password'); // in a real application, hardcoding the user/password would be idiotic\n  // but for the demo application it's very convenient to do so\n\n  if (!usernameEl.val() && !passwordEl.val()) {\n    usernameEl.val('jane_admin');\n    passwordEl.val('kitten');\n  }\n});\n/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ \"./node_modules/jquery/dist/jquery.js\")))\n\n//# sourceURL=webpack:///./assets/js/login.js?");
-
-/***/ })
-
-},[["./assets/js/login.js","runtime","vendors~admin~app~login~search"]]]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([["login"],{"23AV":function(n,a,i){(function(n){n((function(){var a=n("#username"),i=n("#password");a.val()||i.val()||(a.val("jane_admin"),i.val("kitten"))}))}).call(this,i("EVdn"))}},[["23AV","runtime",0]]]);
