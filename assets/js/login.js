f7710163a575cef3e1b41b7883bc582c1ae8252e
Pierre du Plessis <pierre@pcservice.co.za>
2020-01-29 20:09:32 +0200

    Use a more friendly message about hardcoding user details

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,7 +2,7 @@ $(function() {
     var usernameEl = $('#username');
     var passwordEl = $('#password');
 
-    // in a real application, hardcoding the user/password would be idiotic
+    // in a real application, the user/password should never be hardcoded
     // but for the demo application it's very convenient to do so
     if (!usernameEl.val() || 'jane_admin' === usernameEl.val()) {
         usernameEl.val('jane_admin');
