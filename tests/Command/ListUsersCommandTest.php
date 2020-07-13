6fd129c6ccec902ee563272339c7ebb93a8e7b23
Nek (Maxime Veber) <nek.dev@gmail.com>
2020-07-13 16:56:41 +0200

    Add a test for command ListUsers (test emails)

---
+++ tests/Command/ListUsersCommandTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Command;
+
+use App\Command\ListUsersCommand;
+
+class ListUsersCommandTest extends AbstractCommandTest
+{
+    /**
+     * @dataProvider maxResultsProvider
+     *
+     * This test verifies the amount of data is right according to the given parameter max results.
+     */
+    public function testListUsers(int $maxResults): void
+    {
+        $tester = $this->executeCommand(
+            ['--max-results' => $maxResults]
+        );
+
+        $emptyDisplayLines = 5;
+        $this->assertSame($emptyDisplayLines + $maxResults, mb_substr_count($tester->getDisplay(), "\n"));
+    }
+
+    public function maxResultsProvider(): ?\Generator
+    {
+        yield [1];
+        yield [2];
+    }
+
+    public function testItSendsNoEmailByDefault(): void
+    {
+        $this->executeCommand([]);
+
+        $this->assertEmailCount(0);
+    }
+
+    public function testItSendsAnEmailIfOptionProvided(): void
+    {
+        $this->executeCommand(['--send-to' => 'john.doe@symfony.com']);
+
+        $this->assertEmailCount(1);
+    }
+
+    protected function getCommandFqcn(): string
+    {
+        return ListUsersCommand::class;
+    }
+}
