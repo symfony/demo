69d5ff98d3d3e7cd996f9038bfc9ed14202da5e6
Francesco Bianco <info.francescobianco@gmail.com>
2020-10-02 09:43:31 +0200

    replaced external $_SERVER variable with filter_input

--- public/index.php
+++ public/index.php
@@ -9,21 +9,21 @@ require dirname(__DIR__).'/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
-if ($_SERVER['APP_DEBUG']) {
+if (filter_input(INPUT_SERVER, 'APP_DEBUG')) {
     umask(0000);
 
     Debug::enable();
 }
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
+if ($trustedProxies = filter_input(INPUT_SERVER, 'TRUSTED_PROXIES') ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
 }
 
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
+if ($trustedHosts = filter_input(INPUT_SERVER, 'TRUSTED_HOSTS') ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
 }
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel = new Kernel(filter_input(INPUT_SERVER, 'APP_ENV'), (bool) filter_input(INPUT_SERVER, 'APP_DEBUG'));
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
