4fca255a82770bb81c93323da1f0bcbcfbbe6336
Javier Eguiluz <javier.eguiluz@gmail.com>
2021-02-22 09:54:38 +0100

    Recompiled frontend assets

--- public/build/login.js
+++ public/build/login.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["login"],{"23AV":function(n,a,i){(function(n){n((function(){var a=n("#username"),i=n("#password");a.val()&&"jane_admin"!==a.val()||(a.val("jane_admin"),i.val("kitten"))}))}).call(this,i("EVdn"))}},[["23AV","runtime",0]]]);
+(self.webpackChunk=self.webpackChunk||[]).push([[535],{9813:(a,n,e)=>{var l=e(9755);l((function(){var a=l("#username"),n=l("#password");a.val()&&"jane_admin"!==a.val()||(a.val("jane_admin"),n.val("kitten"))}))}},0,[[9813,666,755]]]);
