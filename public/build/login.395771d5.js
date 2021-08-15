497afc07491c66cd6e510359f6be7a46da0e13c4
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-15 17:25:39 +0800

    Delete public directory

--- public/build/login.395771d5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk=self.webpackChunk||[]).push([[535],{348:(a,e,n)=>{var s=n(9755);s((function(){var a=s("#username"),e=s("#password");a.val()&&"jane_admin"!==a.val()||(a.val("jane_admin"),e.val("kitten"))}))}},a=>{"use strict";a.O(0,[755],(()=>{return e=348,a(a.s=e);var e}));a.O()}]);
