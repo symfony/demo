26b6b3db0638ad599b76815ebe4384b5173720c1
Antoine Makdessi <antoine@noni-MacBook-Pro.home>
2021-12-16 15:04:52 +0100

    Add documentation on frontend controllers as application entrypoints

--- public/index.php
+++ public/index.php
@@ -2,7 +2,20 @@
 
 use App\Kernel;
 
+if (!is_file(dirname(__DIR__).'/vendor/autoload_runtime.php')) {
+    throw new LogicException('Symfony Runtime is missing. Try running "composer require symfony/runtime".');
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+/**
+ * This file is a Symfony Front Controller (entrypoint)
+ * when a page is loaded from a browser.
+ *
+ * It boots Symfony in an HTTP Request-Response flow.
+ * 
+ * See https://symfony.com/doc/current/introduction/http_fundamentals.html#the-front-controller
+ */
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
