65ff9cbe83e7764ee24837b248a66f7697e7c84e
Jan Rosier <jan.rosier@gmail.com>
2021-12-23 15:33:36 +0100

    Remove unused parameter

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,7 +15,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
@@ -31,7 +30,7 @@ class SecurityController extends AbstractController
     use TargetPathTrait;
 
     #[Route('/login', name: 'security_login')]
-    public function login(Request $request, Security $security, AuthenticationUtils $helper): Response
+    public function login(Request $request, AuthenticationUtils $helper): Response
     {
         // if user is already logged in, don't display the login page again
         if ($this->getUser()) {
