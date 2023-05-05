37d7a1cd16732f25ff9a71d7a4eeec549be895d7
Javier Eguiluz <javier.eguiluz@gmail.com>
2023-05-05 13:24:41 +0200

    Update the design of the source code modal

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,7 @@ import './styles/app.scss';
 import 'bootstrap/js/dist/alert';
 import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/tab';
 import 'bootstrap/js/dist/modal';
 import 'jquery'
 
