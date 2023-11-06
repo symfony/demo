bc5563707e2db0250365e21eb33dd39b8f0558a0
Antonin <arkanen@gmail.com>
2023-11-06 09:08:04 +0100

    303 redirections symfony/demo#1433

--- src/Controller/Admin/BlogController.php
+++ src/Controller/Admin/BlogController.php
@@ -103,10 +103,10 @@ final class BlogController extends AbstractController
             $submit = $form->get('saveAndCreateNew');
 
             if ($submit->isClicked()) {
-                return $this->redirectToRoute('admin_post_new');
+                return $this->redirectToRoute('admin_post_new', [], Response::HTTP_SEE_OTHER);
             }
 
-            return $this->redirectToRoute('admin_post_index');
+            return $this->redirectToRoute('admin_post_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('admin/blog/new.html.twig', [
@@ -144,7 +144,7 @@ final class BlogController extends AbstractController
             $entityManager->flush();
             $this->addFlash('success', 'post.updated_successfully');
 
-            return $this->redirectToRoute('admin_post_edit', ['id' => $post->getId()]);
+            return $this->redirectToRoute('admin_post_edit', ['id' => $post->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('admin/blog/edit.html.twig', [
@@ -164,7 +164,7 @@ final class BlogController extends AbstractController
         $token = $request->request->get('token');
 
         if (!$this->isCsrfTokenValid('delete', $token)) {
-            return $this->redirectToRoute('admin_post_index');
+            return $this->redirectToRoute('admin_post_index', [], Response::HTTP_SEE_OTHER);
         }
 
         // Delete the tags associated with this blog post. This is done automatically
@@ -177,6 +177,6 @@ final class BlogController extends AbstractController
 
         $this->addFlash('success', 'post.deleted_successfully');
 
-        return $this->redirectToRoute('admin_post_index');
+        return $this->redirectToRoute('admin_post_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -48,7 +48,7 @@ final class UserController extends AbstractController
 
             $this->addFlash('success', 'user.updated_successfully');
 
-            return $this->redirectToRoute('user_edit');
+            return $this->redirectToRoute('user_edit', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('user/edit.html.twig', [
