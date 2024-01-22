bac0c3da3c058b354fd9d2f1a262e8d7536f6363
okorneliuk <oleh.korneliuk@gmail.com>
2024-01-22 12:13:25 +0200

    shortened the function in index.php
    tiny refactoring of index.php

--- public/index.php
+++ public/index.php
@@ -4,6 +4,4 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+return fn (array $context) => new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
